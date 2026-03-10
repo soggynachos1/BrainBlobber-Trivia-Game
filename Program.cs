17dec767453fdf4c4ce2688736bcdad05a96ee8d
Angel Carrillo <147093277+soggynachos1@users.noreply.github.com>
2026-03-09 22:08:49 -0600

    Refactor Program.cs to include BrainBlobber class
    Began the main chunk of the program --angel--

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//main
+class BrainBlobber
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("t");
+    }
+}
